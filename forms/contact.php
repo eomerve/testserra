5814ea384211c346fa3499b6589d6261995a8726
eomerve <merve.celik@ertuncozcan.com>
2023-09-07 14:47:05 +0300

    forms updated second

--- forms/contact.php
+++ forms/contact.php
@@ -1,33 +1,41 @@
 <?php
-// Gerekli kütüphaneyi dahil edin
-require 'class.phpmailer.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-// PHPMailer nesnesini oluşturun
-$mail = new PHPMailer();
+require '../phpmailer/PHPMailer.php';
+require '../phpmailer/Exception.php';
 
-// SMTP ayarları
-$mail->IsSMTP();
-$mail->Host = 'mail.ertuncozcan.com'; // SMTP sunucu adresi
-$mail->Port = 443; // SMTP portu (Varsayılan olarak 25 kullanılır)
-$mail->SMTPAuth = false; // SMTP kimlik doğrulama kullanılıyor mu (true/false)
-$mail->SMTPSecure = ''; // SSL veya TLS kullanılıyor mu ('' veya 'ssl' veya 'tls')
-
-// E-posta gönderen bilgileri
-$mail->From = 'webwp@ertuncozcan.com'; // Gönderen e-posta adresi
-$mail->FromName = 'Ertunç Özcan'; // Gönderenin adı
-
-// Alıcı bilgileri
-$mail->AddAddress('serra.sahin@ertuncozcan.com'); // Alıcı e-posta adresi
-
-// E-posta başlığı ve içeriği
-$mail->Subject = 'Test E-posta'; // E-posta başlığı
-$mail->Body = 'Bu bir test e-postasıdır.'; // E-posta içeriği
-
-// E-postayı gönder
-if($mail->Send()) {
-    echo 'E-posta başarıyla gönderildi.';
-} else {
-    echo 'E-posta gönderilirken hata oluştu: ' . $mail->ErrorInfo;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+    
+    // PHPMailer nesnesini oluşturun
+    $mail = new PHPMailer(true);
+    
+    // SMTP ayarları
+    $mail->isSMTP();
+    $mail->Host = 'mail.ertuncozcan.com'; // SMTP sunucu adresi
+    $mail->Port = 443; // SMTP portu (Varsayılan olarak 25 kullanılır)
+    $mail->SMTPAuth = false; // SMTP kimlik doğrulama kullanılıyor mu (true/false)
+    $mail->Username = 'webwp@ertuncozcan.com'; // SMTP sunucusuna giriş yapacak kullanıcı adı
+    $mail->Password = '71+*gt'; // SMTP sunucusuna giriş yapacak parola
+    
+    // Gönderen ve alıcı bilgileri
+    $mail->setFrom('webwp@ertuncozcan.com', 'Ertunç Özcan'); // Gönderen e-posta ve adı
+    $mail->addAddress('serra.sahin@ertuncozcan.com'); // Alıcı e-posta adresi
+    
+    // E-posta başlığı ve içeriği
+    $mail->Subject = $subject; // E-posta başlığı
+    $mail->Body = "Ad: $name\nE-posta: $email\nKonu: $subject\nMesaj:\n$message"; // E-posta içeriği
+    
+    // E-postayı gönderme işlemi
+    try {
+        $mail->send();
+        echo "E-posta başarıyla gönderildi.";
+    } catch (Exception $e) {
+        echo "E-posta gönderirken bir hata oluştu: " . $mail->ErrorInfo;
+    }
 }
 ?>
-
